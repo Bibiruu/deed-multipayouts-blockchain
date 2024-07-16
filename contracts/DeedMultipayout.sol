b73d321cec808918ddc7c2023f7b080e56c4c6bd
Maria <so.mariagloria@gmail.com>
2024-07-16 19:59:26 +0300

    smart contract done

--- contracts/DeedMultipayout.sol
+++ contracts/DeedMultipayout.sol
@@ -7,10 +7,10 @@ contract DeedMultipayout {
     uint256 public earliest;
     uint256 public amount;
     //gas saving and storing info to code rather than blockchain
-    uint256 public constant PAYOUTS = 10;
+    uint256 public constant MAXPAYOUTS = 10;
     uint256 public constant INTERVAL = 10;
     uint256 public paidPayouts;
-    
+
     constructor(
         address _lawyer,
         address payable _beneficiary,
@@ -20,18 +20,26 @@ contract DeedMultipayout {
         lawyer = _lawyer;
         beneficiary = _beneficiary;
         earliest = block.timestamp + fromNow;
-        amount = msg.value / PAYOUTS;
+        amount = msg.value / MAXPAYOUTS;
     }
 
     function withdraw() public {
         require(msg.sender == beneficiary, "Beneficiary only");
+        //preventing premature withdrawals
         require(block.timestamp >= earliest, "Too early to withdraw");
         //made payouts vs max payouts.
-        require(paidPayouts < PAYOUTS);
-        //how many payouts should we receive
-        uint256 public elligiblePayouts = (block.timestamp - earliest) / INTERVAL;
-
+        require(paidPayouts < MAXPAYOUTS, "No payouts left");
+        //how many payouts should we receive ex. 10 seconds / 10 seconds per interval = 1 payout
+        uint256 elligiblePayouts = (block.timestamp - earliest) / INTERVAL;
+        //example calculation: duepayout 1 = 3-2
+        uint256 duePayouts = elligiblePayouts - paidPayouts;
+        //making sure the transaction matches the amount 
+        duePayouts = duePayouts + paidPayouts > MAXPAYOUTS
+            ? MAXPAYOUTS - paidPayouts
+            : duePayouts;
+        //incrementing payouts
+        paidPayouts += duePayouts;
         //sending everything to the beneficiary
-        beneficiary.transfer(address(this).elligiblePayouts * amount);
+        beneficiary.transfer(duePayouts * amount);
     }
 }
